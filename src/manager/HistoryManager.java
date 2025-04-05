14d3536d9cf0ef2ffca8383c50a9f77370872ffc
Zhexen Zhassulan <zzhexen@gmail.com>
2025-04-06 04:51:06 +0500

    Fix comments from mentor Final -6

--- src/manager/HistoryManager.java
+++ src/manager/HistoryManager.java
@@ -1,11 +1,11 @@
 package manager;
+
 import model.Task;
 import java.util.List;
 
 public interface HistoryManager {
     void addHistory(Task task);
 
-    //void printHistoryId();
     void remove(int id);
 
     List<Task> getHistory();
